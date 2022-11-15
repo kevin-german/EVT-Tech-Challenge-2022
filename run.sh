268909c9000ad33e2ebc5332e666f68c81c9ed5d
Kevin German <98424956+kevin-german@users.noreply.github.com>
2022-11-15 10:41:18 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #Command to generate the key and certificate files
-#openssl req -x509 -newkey rsa:4096 -keyout files/key.pem -out files/cert.pem -sha256 -days 365 -nodes -subj '/CN=localhost'
+#openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj '/CN=localhost'
 
 #Command to run python script
 python3 script.py

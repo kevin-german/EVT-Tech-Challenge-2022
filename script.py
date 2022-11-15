37151df19cd28259027a48f4fc2316a54788be10
Kevin German <98424956+kevin-german@users.noreply.github.com>
2022-11-15 10:37:30 -0600

    Add files via upload

---
+++ script.py
@@ -0,0 +1,31 @@
+#--Imports (from standard library)
+import http.server
+import socketserver
+import ssl
+import socket
+
+#--Specifying port and creating handler
+PORT = 5000
+Handler = http.server.SimpleHTTPRequestHandler
+
+#--Serving
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+#--Section to wrap the socket for self signed certificate
+'''
+with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
+    httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="files/key.pem", certfile="files/cert.pem", server_side=True, ssl_version=ssl.PROTOCOL_TLS)
+    print("serving at port", PORT)
+    httpd.serve_forever()
+'''
+
+
+
+
+
+
+
+
+
